74761c11e29c74376eae5c21313e4b9fef129b8a
Jeff Stewart <stewarjs@gmail.com>
2020-05-17 15:54:50 -0500

    access doc metadata

--- crawler-request-js.js
+++ crawler-request-js.js
@@ -108,6 +108,7 @@ function _crawler_request(current_url) {
 			} else if (ret.type == "doc") {
 				OFFICEPROPS.getData(data).then(function(metadata){
                     ret.metadata = metadata;
+                    return ret;
                 }).catch(err => {
 						ret.status = -222;
 						ret.error = err.toString();
