a8d439e52727daef456dae3a24175be656541975
Jeff Stewart <stewarjs@gmail.com>
2020-05-16 20:43:56 -0500

    switch back to returning metadata

--- crawler-request-js.js
+++ crawler-request-js.js
@@ -101,7 +101,7 @@ function _crawler_request(current_url) {
 				return PdfParse(data)
 					.then(res => {
 						if (res) {
-							ret.metadata = res.info;
+							ret.metadata = res.metadata;
 							ret.text = res.text;
 						}
 
